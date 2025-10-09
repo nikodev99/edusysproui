c4dd1898a9a654f7814cfb01660fe530d051d5ff
Nikhe Niama <nikhe.niama99@outlook.fr>
2025-10-09 22:18:24 +0200

    Feat: Added `OutletPage` component with breadcrumb support, dynamic content rendering, and response message handling

---
+++ src/pages/OutletPage.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "../core/utils/interfaces.ts";
+import {useDocumentTitle} from "../hooks/useDocumentTitle.ts";
+import {BCProps, useBreadCrumb} from "../hooks/useBreadCrumb.tsx";
+import {ReactNode} from "react";
+import FormSuccess from "../components/ui/form/FormSuccess.tsx";
+import FormError from "../components/ui/form/FormError.tsx";
+import {Alert} from "antd";
+
+export interface OutletPageProps {
+    metadata: Metadata
+    breadCrumb: BCProps
+    content: ReactNode
+    responseMessages?: {success?: ReactNode, error?: ReactNode}
+    redirectLink?: string
+    isNotif?: boolean
+}
+
+const OutletPage = ({metadata, breadCrumb, content, responseMessages, redirectLink, isNotif = true}: OutletPageProps) => {
+    useDocumentTitle(metadata)
+
+    const {context} = useBreadCrumb(breadCrumb)
+
+    return <>
+        {context}
+        {responseMessages && (
+            <>
+                {responseMessages?.success && <FormSuccess message={responseMessages?.success} redirectLink={redirectLink} isNotif={isNotif}/>}
+                {responseMessages?.error && <FormError message={responseMessages?.error} isNotif={isNotif}/>}
+
+                {responseMessages?.success && <Alert type={'success'} message={responseMessages?.success} closeIcon showIcon style={{marginBottom: '10px'}}/>}
+                {responseMessages?.error && <Alert type={'error'} message={responseMessages?.error} closeIcon showIcon style={{marginBottom: '10px'}}/>}
+            </>
+        )}
+        {content}
+    </>
+}
+
+export default OutletPage
